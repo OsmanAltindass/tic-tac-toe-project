a461eecbc8b4679f295dbd2fe705c0e776fbe474
OsmanAltindass <51475729+OsmanAltindass@users.noreply.github.com>
2024-06-12 12:18:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TicTacToe game = new TicTacToe();
+        game.start();
+        game.play();
     }
-
-    //kommentar
-
-
-
 }
